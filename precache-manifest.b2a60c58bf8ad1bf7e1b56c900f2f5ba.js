9b8d962addf8ca4ca86bb53965465aa682b08eba
root <Laneyang>
2021-05-28 03:00:23 -0700

    Updates

---
+++ precache-manifest.b2a60c58bf8ad1bf7e1b56c900f2f5ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e264973929110e08b46cb8c750e4c05e",
+    "url": "/LaneYang/cv.github.io/index.html"
+  },
+  {
+    "revision": "f58b4e3711aaf2302e88",
+    "url": "/LaneYang/cv.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "51bf5c8fb2f60980930e",
+    "url": "/LaneYang/cv.github.io/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "f58b4e3711aaf2302e88",
+    "url": "/LaneYang/cv.github.io/static/js/2.7bc6c8f7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/LaneYang/cv.github.io/static/js/2.7bc6c8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51bf5c8fb2f60980930e",
+    "url": "/LaneYang/cv.github.io/static/js/main.71f0ee1d.chunk.js"
+  },
+  {
+    "revision": "a5490cc8fd982a921a2e",
+    "url": "/LaneYang/cv.github.io/static/js/runtime-main.0c2df5af.js"
+  }
+]);
